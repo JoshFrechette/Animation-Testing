414f80d6f9781f7d4c4959d6a8048987b507f360
Josh Frechette <josh.f.design@gmail.com>
2020-03-10 15:34:59 -0400

    Basic functioning animation.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import Wrapper from './components/Wrapper.js';
-import Sandbox from './components/Sandbox.js';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Wrapper from './components/Wrapper.js';
+// import Sandbox from './components/Sandbox.js';
+import World from './components/World';
+import { PlayerProvider } from './contexts/PlayerContext';
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <Wrapper>
+  //       <Sandbox />
+  //     </Wrapper>
+  //   </div>
+  // );
   return (
-    <div className="App">
-      <Wrapper>
-        <Sandbox />
-      </Wrapper>
-    </div>
+    <Router>
+
+      <div id="app">
+        <NavBar />
+
+        <div class="row">
+            <Switch>
+                <Route exact path='/'/>
+                <Route exact path='/userinfo'/>
+                <Route exact path='/game'>
+                  <PlayerProvider>
+                    <World />
+                  </PlayerProvider>
+                </Route>
+                {/* <PrivateRoute path='/profile' component={Profile} /> */}
+              </Switch>
+            </div>
+        </div>
+
+        {/* <Footer /> */}
+    </Router>
   );
 }
 
